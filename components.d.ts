4177870039d0e6d11262e6cd79f1eb98e75e9396
sunpm <15022352@qq.com>
2022-08-22 14:26:22 +0800

    实现拖拽，并在控制台打印拖拽改变回调

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,9 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <script setup lang="ts">
 import { useStorage } from '@vueuse/core'
+import {ref} from "vue"
+import draggable from 'vuedraggable'
 import {postDB} from "./db/post"
 
 // 初始化数据
 const state = useStorage('post', postDB, localStorage)
+const drag = ref(false)
 
+function log(value) {
+  console.log(value)
+}
 </script>
 
 <template>
-
+  <draggable
+      class="wrapper"
+      :modelValue="state.data"
+      @start="drag = true"
+      @end="drag = false"
+      item-key="index"
+      @change="log"
+  >
+    <template #item="{ element }">
+      <div class="item">
+        <p>{{ element.name }}</p>
+      </div>
+    </template>
+  </draggable>
 </template>
 
 <style scoped>
-
+.wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.item {
+  width: 100px;
+  height: 100px;
+  font-size: 20px;
+  text-align: center;
+  line-height: 100px;
+  margin: 10px;
+  background-color: #42b983;
+  color: #ffffff;
+}
 </style>
