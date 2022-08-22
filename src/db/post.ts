558ab892b0696f38b6d2f8ec07026c67734f6cbc
sunpm <15022352@qq.com>
2022-08-22 11:43:25 +0800

    添加假数据

---
+++ src/db/post.ts
@@ -0,0 +1,22 @@
+export const postDB = [
+  {
+    id: 1,
+    name: '文章1'
+  },
+  {
+    id: 2,
+    name: '文章2'
+  },
+  {
+    id: 3,
+    name: '文章3'
+  },
+  {
+    id: 4,
+    name: '文章4'
+  },
+  {
+    id: 5,
+    name: '文章5'
+  },
+]
