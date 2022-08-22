27869a91a459426c1347a169caa5eabcb6b97a04
sunpm <15022352@qq.com>
2022-08-22 11:53:12 +0800

    修改假数据数据

--- src/db/post.ts
+++ src/db/post.ts
@@ -1,22 +1,24 @@
-export const postDB = [
-  {
-    id: 1,
-    name: '文章1'
-  },
-  {
-    id: 2,
-    name: '文章2'
-  },
-  {
-    id: 3,
-    name: '文章3'
-  },
-  {
-    id: 4,
-    name: '文章4'
-  },
-  {
-    id: 5,
-    name: '文章5'
-  },
-]
+export const postDB = {
+  data: [
+    {
+      id: 1,
+      name: '文章1'
+    },
+    {
+      id: 2,
+      name: '文章2'
+    },
+    {
+      id: 3,
+      name: '文章3'
+    },
+    {
+      id: 4,
+      name: '文章4'
+    },
+    {
+      id: 5,
+      name: '文章5'
+    },
+  ]
+}
